0a8209439e49dfac974da0c06fcbe779dc7ffb01
Anne Sannella <27774757+Annesannella1@users.noreply.github.com>
2023-05-13 20:14:01 -0500

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,31 @@
+$(function() {
+  
+  $('[data-scrollmagic]').each(function (index, elem) {
+    // Init ScrollMagic Controller
+    var scrollMagicController = new ScrollMagic();
+  
+     
+    // Create Animations
+    var title = $(elem).find('h3'),
+           btn = $(elem).find('a');
+        
+    
+    var tl = new TimelineMax({pause: true});    
+    tl.add("start") 
+      .fromTo(title, 0.4, { y: '40px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+      
+      .fromTo(btn, 0.4, { y: '100px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+    
+    
+    // Create the Scene and trigger when visible
+    var scene = new ScrollScene({
+      triggerElement: elem,
+      offset: 0
+    })
+    .setTween(tl)
+    .addTo(scrollMagicController);
+
+    // Add debug indicators fixed on right side
+     scene.addIndicators(); 
+  });
+});
