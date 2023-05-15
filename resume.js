f7f40341f749300a46c53cdbdeb80e19f3054a50
Anne Sannella <27774757+Annesannella1@users.noreply.github.com>
2023-05-15 18:56:47 -0500

    Update resume.js

--- resume.js
+++ resume.js
@@ -6,14 +6,13 @@ $(function() {
   
      
     // Create Animations
-    var title = $(elem).find('h3'), text = $(elem).find('h2'), bulltets = $(elem).find('ul li'), btn = $(elem).find('a');
+    var title = $(elem).find('h3'), text = $(elem).find('h2 ul li'), btn = $(elem).find('a');
         
     
     var tl = new TimelineMax({pause: true});    
     tl.add("start") 
       .fromTo(title, 0.4, { y: '40px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
       .fromTo(text, 0.4, { y: '60px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
-      .fromTo(bullets, 0.4, { y: '60px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
       .fromTo(btn, 0.4, { y: '100px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
     
     
